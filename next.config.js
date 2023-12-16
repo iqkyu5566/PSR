88d0874aad08c9f9f9350ee7e157059931756da6
zikrifikri21 <iniakunku329@gmail.com>
2023-12-16 19:15:16 +0800

    aowksok

--- next.config.js
+++ next.config.js
@@ -1,28 +1,15 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
-
-// const withTM = require('next-transpile-modules')(['@babel/preset-react']);
-//   '@fullcalendar/common',
-//   '@fullcalendar/common',
-//   '@fullcalendar/daygrid',
-//   '@fullcalendar/interaction',
-//   '@fullcalendar/react',
-
 const nextConfig = {
+  reactStrictMode: true,
   swcMinify: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   images: {
-    domains: [
-      'images.unsplash.com',
-      'i.ibb.co',
-      'scontent.fotp8-1.fna.fbcdn.net',
-    ],
-    // Make ENV
-    unoptimized: true,
+    domains: ['img.pokemondb.net'],
   },
-  experimental: {
-    appDir: true,
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- src/api/v1/route.js
+++
@@ -1,6 +0,0 @@
-import { axios } from './../../../node_modules/axios/dist/esm/axios';
-const apiProject = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-});
-
-export default apiProject;
